c0eaac3289dd76d7109f26ff650b359faa839b66
mehul goswami <mehulgoswami@mehuls-macbook-air.local>
2017-02-04 18:23:29 +0530

    Change String

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let stringVAlue = "Hello world"
+        print(stringVAlue)
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
     }
 
 
