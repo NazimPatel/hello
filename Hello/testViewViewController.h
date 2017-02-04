aa12cfe9a15af64c15245e1266a0d9be1ec3d04d
mehul goswami <mehulgoswami@mehuls-macbook-air.local>
2017-02-04 18:44:50 +0530

    add new controller

---
+++ Hello/testViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  testViewViewController.h
+//  Hello
+//
+//  Created by mehul goswami on 04/02/17.
+//  Copyright © 2017 mehul goswami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testViewViewController : UIViewController
+
+@end
---
+++ Hello/testViewViewController.m
@@ -0,0 +1,37 @@
+//
+//  testViewViewController.m
+//  Hello
+//
+//  Created by mehul goswami on 04/02/17.
+//  Copyright © 2017 mehul goswami. All rights reserved.
+//
+
+#import "testViewViewController.h"
+
+@interface testViewViewController ()
+
+@end
+
+@implementation testViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
